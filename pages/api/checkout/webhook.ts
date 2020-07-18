5c9777127cc6e89eaf84835a5dbb42f61675455c
Kit Field <christopher.field@radiuspaymentsolutions.com>
2020-07-18 10:24:45 +0100

    Add signature verification to the checkout webhook

--- pages/api/checkout/webhook.ts
+++ pages/api/checkout/webhook.ts
@@ -1,18 +1,58 @@
+
+import { buffer } from 'micro'
+import Cors from 'micro-cors'
 import { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe';
 import setProductQuantity from '../../../services/graphcms/set-product-quantity';
 
-type Data = {};
+const webhookSecret: string = process.env.STRIPE_WEBHOOK_SECRET;
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: '2020-03-02',
 });
 
-export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  if (req.body.type === 'checkout.session.completed') {
-    const sessionId = req.body.data.object.id;
-    const { data } = await stripe.checkout.sessions.listLineItems(sessionId as string);
-    await setProductQuantity(data);
+// Stripe requires the raw body to construct the event.
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const cors = Cors({
+  allowMethods: ['POST', 'HEAD'],
+});
+
+const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const buf = await buffer(req)
+    const sig = req.headers['stripe-signature']!
+
+    let event: Stripe.Event
+
+    try {
+      event = stripe.webhooks.constructEvent(buf.toString(), sig, webhookSecret)
+    } catch (err) {
+      // On error, log and return the error message.
+      console.log(`Webhook Error: ${err.message}`)
+      res.status(400).send(`Webhook Error: ${err.message}`)
+      return
+    }
+
+    // Cast event data to Stripe object.
+    if (event.type === 'checkout.session.completed') {
+      const checkoutSession = event.data.object as Stripe.Checkout.Session;
+      const {id: sessionId} = checkoutSession;
+      const { data } = await stripe.checkout.sessions.listLineItems(sessionId as string);
+      await setProductQuantity(data); 
+    } else {
+      console.warn(`Unhandled event type: ${event.type}`)
+    }
+    // Return a response to acknowledge receipt of the event.
+    res.json({ received: true })
+  } else {
+    res.setHeader('Allow', 'POST')
+    res.status(405).end('Method Not Allowed')
   }
-  res.status(200).json({});
 }
+
+export default cors(webhookHandler as any);
